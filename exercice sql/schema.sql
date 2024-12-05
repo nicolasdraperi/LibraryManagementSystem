116b2a7bfd6a9acc2ee61f7df395fa05a733cac8
Nicolas Draperi <nicolas.draperi2@gmail.com>
2024-12-05 12:29:24 +0100

    Task 1 sql

---
+++ exercice sql/schema.sql
@@ -0,0 +1,25 @@
+/*
+create database exercicesql;
+use exercicesql;
+*/
+create table students (
+    student_id int primary key,
+    name varchar(50),
+    age int,
+    gender varchar(10)
+);
+
+create table courses (
+    course_id int primary key,
+    course_name varchar(50),
+    credits int,
+    capacity int
+);
+
+create table enrollments (
+    enrollment_id int primary key auto_increment,
+    student_id int,
+    course_id int,
+    foreign key (student_id) references students(student_id),
+    foreign key (course_id) references courses(course_id)
+);
