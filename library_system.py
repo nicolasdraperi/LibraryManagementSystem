375c76826b884fbe99ec5909bea01ecaf82cb810
Nicolas Draperi <nicolas.draperi2@gmail.com>
2024-12-05 11:40:59 +0100

    Task 4

--- library_system.py
+++ library_system.py
@@ -7,9 +7,11 @@ class Book:
     def __str__(self):
         return "title: " + self.title + "\nauthor: " + self.author + "\nis available: " + str(self.is_Available)
 
+
 class Library:
     def __init__(self):
         self.books = []
+
     def add_book(self, title: str, author: str):
         self.books.append(Book(title, author))
 
@@ -26,11 +28,40 @@ class Library:
                     self.add_book(title.strip(), author.strip())
 
 
+class Student:
+    def __init__(self, name: str):
+        self.name = name
+        self.borrowed_books = []
+
+    def borrow_book(self, book_title: str, library: Library):
+        for book in library.books:
+            if book.title == book_title and book.is_Available:
+                book.is_Available = False
+                self.borrowed_books.append(book)
+                break
+
+    def return_book(self, book_title: str, library: Library):
+        for book in library.books:
+            if book.title == book_title:
+                book.is_Available = True
+                self.borrowed_books.remove(book)
+                break
+
 
 lib = Library()
 lib.load_books("library_data.txt")
 
+student = Student("John Doe")
+student.borrow_book("1984", lib)
+
 books = lib.list_books()
 for book in books:
     print(book)
-    print("---------")
+    print('----')
+
+student.return_book("1984", lib)
+
+for book in books:
+    print(book)
+    print('----')
+
