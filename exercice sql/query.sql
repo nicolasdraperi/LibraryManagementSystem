731b1ffe7398e0c1ad606e553d13df87c4704b69
Nicolas Draperi <nicolas.draperi2@gmail.com>
2024-12-05 12:38:29 +0100

    Task 4 sql

--- exercice sql/query.sql
+++ exercice sql/query.sql
@@ -19,3 +19,29 @@ left join
     enrollments e on s.student_id = e.student_id
 where
     e.enrollment_id is null;
+
+/*Task 4*/
+
+select
+    c.course_name,
+    count(e.enrollment_id) as nb_students
+from
+    courses c
+left join
+    enrollments e on c.course_id = e.course_id
+group by
+    c.course_id, c.course_name;
+
+
+select
+    c.course_name,
+    count(e.enrollment_id) as enrolled_students,
+    c.capacity
+from
+    courses c
+join
+    enrollments e on c.course_id = e.course_id
+group by
+    c.course_id, c.course_name, c.capacity
+having
+    count(e.enrollment_id) > c.capacity / 2;
